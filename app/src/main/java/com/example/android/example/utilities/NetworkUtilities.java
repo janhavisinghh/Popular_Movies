f17122d6599bd32f807f129d33c4d7ce0c6a17f3
Janhavi Singh <janhavi.shanu@gmail.com>
2018-07-27 21:23:41 +0530

    android permission

---
+++ app/src/main/java/com/example/android/example/utilities/NetworkUtilities.java
@@ -0,0 +1,59 @@
+package com.example.android.example.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtilities {
+    final static String POPMOV_BASE_URL =
+            "https://api.themoviedb.org/3/movie/popular";
+
+    final static String PARAM_API_KEY = "api_key";
+
+    final static String PARAM_LANG = "language";
+    final static String lang = "en-US";
+
+
+    public static URL buildUrl() {
+        String api_key = "@string/api_key" ;
+
+        Uri builtUri = Uri.parse(POPMOV_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, api_key)
+                .appendQueryParameter(PARAM_LANG, lang)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
